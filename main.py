64cddcbb989377bc670d4f73ed0c1df91088d6f2
lucas hissinger <lucas.hissinger@gmail.com>
2022-05-12 14:11:26 +0200

    [ADD]add auto reply and tweet at certain time

--- main.py
+++ main.py
@@ -7,14 +7,25 @@
 
 import tweepy as tp
 import time
+import datetime
 import os
 from create_img import create_img
 
-def tweet():
-    create_img()
-    media = api.media_upload("final_img.png")
-    tweet = ""
-    post_result = api.update_status(status=tweet, media_ids=[media.media_id])
+def tweet(dt_now):
+    if now.strftime("%H:%M:%S") == "14:00:30":
+        print("TWEET!")
+        create_img()
+        media = api.media_upload("final_img.png")
+        tweet = ""
+        post_result = api.update_status(status=tweet, media_ids=[media.media_id])
+        time.sleep(1)
+
+def reply():
+    print("reply")
+    tweets = api.mentions_timeline(1, tweet_mode = 'extended')
+    for tweet in tweets:
+        #api.update_status(status="@" + tweet.user.screen_name + " " + "Hello I'm a bot in development", in_reply_to_status_id=tweet.id)
+        print(str(tweet.id) + ' - ' + tweet.text)
 
 
 # credentials to login to twitter api
@@ -29,6 +40,6 @@ auth.set_access_token(access_token, access_secret)
 api = tp.API(auth)
 
 while True:
-    print("TWEET!")
-    tweet()
-    time.sleep(60)
+    now = datetime.datetime.now()
+    tweet(now)
+    # reply()
