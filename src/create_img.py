e3c467bff1cb4ea46f926c7dc93da8be2b76e5a6
lucas hissinger <lucas.hissinger@gmail.com>
2022-06-23 10:34:16 +0200

    [ADD]orga repo and try to download video media

---
+++ src/create_img.py
@@ -0,0 +1,54 @@
+##
+## PERSONAL PROJECT 2022
+## python script to make img
+## File description:
+## main
+##
+
+from PIL import Image, ImageDraw, ImageFont
+from random import choice, randint
+from os import remove
+
+def read_and_set(filename):
+    lines = []
+    with open(filename) as f:
+        lines = f.readlines()
+    f.close()
+    return choice(lines).rstrip("\n")
+
+def random_coords(txt_id):
+    if txt_id == 1:
+        x = randint(10, 400)
+        y = randint(10, 400)
+        return (x, y)
+    elif txt_id == 2:
+        x = randint(10, 400)
+        y = randint(400, 750)
+        return (x, y)
+    else:
+        print("Invalid txt_id")
+        exit(0)
+
+def create_img():
+
+    colors_pastel = ['#F9E4D6', '#F5CBE0', '#D4BDE7', '#CEDDEB', '#E8EBD5']
+    colors = ['red', 'green', 'blue', 'orange', 'purple', 'black']
+    font = ['../font/Medium.ttf', '../font/Semi.ttf', '../font/Regular.ttf', '../font/Thin.ttf']
+    fnt1 = ImageFont.truetype(choice(font), 55)
+    fnt2 = ImageFont.truetype(choice(font), 55)
+
+    question = read_and_set('../txt/questions.txt')
+    answer = read_and_set('../txt/response.txt')
+    img = Image.new('RGB', (1920, 1080), color = choice(colors_pastel))
+    d = ImageDraw.Draw(img)
+    d.text(random_coords(1), question, font=fnt1, fill=choice(colors))
+    d.text(random_coords(2), answer, font=fnt2, fill=choice(colors))
+    img.save('base_img.png')
+
+    Image1 = Image.open('base_img.png')
+    Image1copy = Image1.copy()
+    Image2 = Image.open('pic/' + str(randint(1,6)) + '.png')
+    Image2copy = Image2.copy()
+    Image1copy.paste(Image2copy, (randint(0, 1500), randint(200, 800)))
+    Image1copy.save('final_img.png') # save the image
+    remove('base_img.png')
---
+++ src/methods.py
@@ -0,0 +1,140 @@
+##
+## PERSONAL PROJECT, 2022
+## bot
+## File description:
+## methods
+##
+
+import time
+import tweepy as tp
+from create_img import create_img, read_and_set
+from anime import *
+import requests
+import json
+import csv
+import os
+
+def read_last_id():
+    file = open("../txt/last_seen.txt", "r")
+    last_id = int(file.read().strip())
+    file.close()
+    return last_id
+
+def store_last_id(last_id):
+        file = open("../txt/last_seen.txt", "w")
+        file.write(str(last_id))
+        file.close()
+        return;
+
+def tweet(api, dt_now):
+    tweet = 0
+    if dt_now.strftime("%H:%M:%S") == "15:19:10":
+        tweet = 1
+    if tweet == 1:
+        print("TWEET!")
+        create_img()
+        media = api.media_upload("final_img.png")
+        tweet = "Daily post"
+        api.update_status(status=tweet, media_ids=[media.media_id])
+        tweet = 0
+        os.remove("final_img.png")
+
+def coiffeur(api, tweet):
+    str = tweet.text[::-1]
+    tmp = ""
+    for i in range(len(str)):
+        if str[i].isalpha():
+            tmp += str[i]
+        if len(tmp) == 4: break
+    if tmp[::-1].lower() == 'quoi':
+        response = response = "@" + tweet.user.screen_name + " Feur"
+        api.update_status(status=response, in_reply_to_status_id=tweet.id)
+    elif 'quoi' in tweet.text.lower():
+        response = response = "@" + tweet.user.screen_name + " Je pense que feur"
+        api.update_status(status=response, in_reply_to_status_id=tweet.id)
+
+def anime(api, tweet):
+    if '#anime' in tweet.text.lower():
+        link = anime_main(tweet.text)
+        if link == "$ERROR$":
+            api.update_status(status="@" + tweet.user.screen_name + " Je ne connais pas cet animé :(\n N'hesitez pas a m'en faire part dans mes DM", in_reply_to_status_id=tweet.id)
+            return;
+        media = api.media_upload("anime_syn.png")
+        api.update_status(status="@" + tweet.user.screen_name + " for more info : " + link, in_reply_to_status_id=tweet.id, media_ids=[media.media_id])
+
+def download_video(api, tweet):
+    infos = tweet.entities.get('media', [])
+    print(infos)
+    if infos != []:
+        url = infos[0]['media_url']
+        print("url : " + url)
+        wget.download(url, "../media/video.mp4")
+        # api.update_status(status="@" + tweet.user.screen_name + " " + url, in_reply_to_status_id=tweet.id)
+
+def reply(api):
+    tweets = api.mentions_timeline(count=1)
+    time.sleep(5)
+    for tweet in (tweets):
+        print("tweet : " + tweet.text + " by : " + tweet.user.screen_name)
+        if (read_last_id() != tweet.id):
+            print("reply !")
+            coiffeur(api, tweet)
+            message_dm(api, tweet)
+            show_help(api, tweet)
+            get_meteo(api, tweet)
+            anime(api, tweet)
+            download_video(api, tweet)
+            # api.create_favorite(tweet.id)
+            store_last_id(tweet.id)
+
+def message_dm(api, tweet):
+    if '#dm' in tweet.text.lower():
+        print("DM !")
+        message = read_and_set('../txt/citations_life.txt')
+        message += "\nby : me :)"
+        api.send_direct_message(tweet.user.id, message)
+
+def show_help(api, tweet):
+    if '#help' in tweet.text.lower():
+        print("help !")
+        message = ""
+        with open("../txt/help.txt") as f:
+            message = f.read()
+        f.close()
+        api.send_direct_message(tweet.user.id, message)
+        response = "@" + tweet.user.screen_name + " Check your DM now ! :D"
+        api.update_status(status=response, in_reply_to_status_id=tweet.id)
+
+def get_city(string):
+
+    file = open("../txt/cities.csv", "r")
+    data = csv.reader(file, delimiter=',')
+    names = []
+    for row in data:
+        names.append(row[0])
+    for name in names:
+        if name in string:
+            return name
+    return "84"
+
+def get_meteo(api, tweet):
+    if '#meteo' in tweet.text.lower():
+        ville = get_city(tweet.text)
+        if ville == "84":
+            api.update_status(status="@" + tweet.user.screen_name + " Je ne connais pas cette ville :(\n Vous êtes sûr de l'orthographe ?", in_reply_to_status_id=tweet.id)
+            return;
+        url_weather = os.getenv('URL_METEO') + ville + os.getenv('API_METEO')
+        r_weather = requests.get(url_weather)
+        data = r_weather.json()
+        message = "Vous etes a " + ville
+        t = data['main']['temp']
+        message += "\nLa temperature moyenne est de " + str(round(t - 273.15, 2)) + " degres Celsius"
+        t_min = data['main']['temp_min']
+        t_max = data['main']['temp_max']
+        message += "\nLes temperatures varient entre " + str(round(t_min - 273.15, 2)) + " et " + str(round(t_max - 273.15, 2)) + " degres Celsius"
+        humidite = data['main']['humidity']
+        message += "\nLe taux d'humidite est de " + str(humidite) + "%"
+        temps = data['weather'][0]['description']
+        message += "\nConditions climatiques : " + str(temps)
+        api.update_status(status="@" + tweet.user.screen_name + " " + message, in_reply_to_status_id=tweet.id)
+        time.sleep(1)
